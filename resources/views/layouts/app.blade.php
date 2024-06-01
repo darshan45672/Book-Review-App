fa5c07c7d9c774f5b14e1744598b9feedf8226ff
darshan45672 <drshnbhandary@gmail.com>
2024-06-01 12:23:05 +0530

    implemented show book detail page with dynamic loading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,9 +16,9 @@
 <body class="bg-light">
     <div class="container-fluid shadow-lg header">
         <div class="container">
-            <div class="d-flex justify-content-between">
+            <div class="p-3 d-flex justify-content-between">
                 <h1 class="text-center"><a href="{{ route('home') }}" class="h3 text-white text-decoration-none">Book Review App</a></h1>
-                <div class="d-flex align-items-center navigation">
+                <div class="d-flex gap-5 align-items-center navigation">
                     @if (Auth::check())
                         <a href="{{ route('account.showProfile') }}" class="text-white">My Account</a>
                     @else    
